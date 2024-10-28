f65bfbb1be4005482082c6118466bf1e2f87c622
Akshay Kumar Jha <akshay300899@gmail.com>
2024-10-28 19:14:04 +0530

    Bottom tab customization

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,37 @@
 import { store } from "@/store/store";
 import { Tabs } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Provider } from "react-redux";
 
 export default function TabLayout() {
   return (
     <Provider store={store}>
       <Tabs>
-        <Tabs.Screen name="index" options={{ title: "feed" }} />
-        <Tabs.Screen name="post" />
-        <Tabs.Screen name="profile" />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "feed",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="feed" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="post"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="send" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="account-circle" size={24} color={color} />
+            ),
+          }}
+        />
       </Tabs>
     </Provider>
   );
